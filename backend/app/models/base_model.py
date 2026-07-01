eb612bcbc96cb170c91cb7861761744111b22e5b
JulyAnahi <132855524+JulyAnahi@users.noreply.github.com>
2026-07-01 07:23:10 -0300

    Delete backend directory

--- backend/app/models/base_model.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from app.database import db
-
-class BaseModel(db.Model):
-    __abstract__ = True 
-
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
--- backend/app/models/rol.py
+++
@@ -1,8 +0,0 @@
-from app.database import db
-from app.models.base_model import BaseModel
-
-class Rol(BaseModel):
-    __tablename__ = 'roles'
-
-    nombre = db.Column(db.String(50), nullable=False, unique=True)
-    users = db.relationship('User', backref='rol', lazy=True)
