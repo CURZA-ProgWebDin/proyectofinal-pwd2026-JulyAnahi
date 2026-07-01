8d5f98a5399619ed353061d1be2c515399aa04f2
JulyAnahi <132855524+JulyAnahi@users.noreply.github.com>
2026-07-01 07:23:24 -0300

    Delete frontend directory

--- frontend/src/services/movimientosService.js
+++
@@ -1,6 +0,0 @@
-import api from './api'
-export default {
-  listarTodos() { return api.get('/movimientos/') },
-  listarMisMovimientos() { return api.get('/movimientos/mis/') },
-  registrar(data) { return api.post('/movimientos/', data) }
-}
